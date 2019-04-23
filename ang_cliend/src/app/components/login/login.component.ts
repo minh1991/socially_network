974890bd1e08536edce2368f366aeeebea312260
minhlq2 <minhlq2@rikkeisoft.com>
2019-04-23 08:26:33 +0700

    23/04/2019 - Login/ end

--- ang_cliend/src/app/components/login/login.component.ts
+++ ang_cliend/src/app/components/login/login.component.ts
@@ -25,5 +25,22 @@ export class LoginComponent implements OnInit {
     });
   }
 
-  loginUser() {}
+  loginUser() {
+    this.showLoading = true;
+    console.log(this.loginForm.value);
+    this.authenticationService.loginUser(this.loginForm.value).subscribe(
+      data => {
+        console.log(data);
+        this.loginForm.reset();
+        setTimeout(() => {
+          this.router.navigate(['feed']);
+        }, 1500);
+      },
+      err => {
+        this.showLoading = false;
+        // console.log(err.error.message);
+        this.errorMessage = err.error.message;
+      }
+    );
+  }
 }
