c1991e11595cd5249616a5e8b7a8752709470125
minh1991 <hikari.mei.119@gmail.com>
2019-06-24 08:56:16 +0700

    remove display userLogging

--- ang_cliend/src/app/components/friend-actions/friend/friend.component.ts
+++ ang_cliend/src/app/components/friend-actions/friend/friend.component.ts
@@ -1,5 +1,7 @@
+import { TokenService } from './../../../services/token.service';
 import { Component, OnInit } from '@angular/core';
 import { UsersService } from '../../../services/users.service';
+import * as _ from 'lodash'
 
 @Component({
   selector: 'app-friend',
@@ -8,15 +10,21 @@ import { UsersService } from '../../../services/users.service';
 })
 export class FriendComponent implements OnInit {
   users: [];
-  constructor(private userService: UsersService) {}
+  loggingUser: any
+  constructor(
+    private userService: UsersService,
+    private tokenService: TokenService
+  ) { }
 
   ngOnInit() {
+    this.loggingUser = this.tokenService.GetPayLoad()
     this.GetAllUsers();
   }
 
   GetAllUsers() {
     this.userService.GetAllUsers().subscribe(data => {
       // console.log('Fr Item data--', data.allUsers);
+      _.remove(data.allUsers, { username: this.loggingUser.username })
       this.users = data.allUsers;
       console.log('users--', this.users);
     });
